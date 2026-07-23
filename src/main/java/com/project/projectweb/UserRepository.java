5f0f8457a06371236fe21dfc2eb33fc381890040
jgcavs <jgcavalcanti18@gmail.com>
2026-07-23 13:25:47 -0300

    registro de componente

--- src/main/java/com/project/projectweb/UserRepository.java
+++ src/main/java/com/project/projectweb/UserRepository.java
@@ -3,5 +3,6 @@ package com.project.projectweb;
 import com.project.projectweb.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
